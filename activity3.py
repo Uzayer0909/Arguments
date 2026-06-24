a0867c7a8ba58e3cd9b0643d1519f987f4dfdc03
Uzayer0909 <uzairsayed205@gmail.com>
2026-06-24 20:08:23 +0600

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,15 @@
+def factorial(number):
+
+
+    if number == 0 or number == 1:
+        return 1
+    else:
+        return number*factorial(number-1)
+    
+
+
+print(factorial(0))
+print(factorial(3))
+print(factorial(4))
+print(factorial(5))
+print(factorial(6))
